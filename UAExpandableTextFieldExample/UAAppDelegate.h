bfe06d9089807130f2b1c084da436047d1ee8cca
Umair Aamir <umair.amir@confiz.com>
2014-05-07 17:16:37 +0500

    Initial Commit

---
+++ UAExpandableTextFieldExample/UAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UAAppDelegate.h
+//  UAExpandableTextFieldExample
+//
+//  Created by Umair Aamir on 5/7/14.
+//  Copyright (c) 2014 Confiz Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UAExpandableTextFieldExample/UAViewController.h
@@ -0,0 +1,13 @@
+//
+//  UAViewController.h
+//  UAExpandableTextFieldExample
+//
+//  Created by Umair Aamir on 5/7/14.
+//  Copyright (c) 2014 Confiz Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UAViewController : UIViewController
+
+@end
